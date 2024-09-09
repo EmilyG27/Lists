9440844bba0fb2d537b19cbd59869846c1e7eebc
EmilyG27 <133466995+EmilyG27@users.noreply.github.com>
2024-09-08 23:15:37 -0500

    Add files via upload

---
+++ Advanced list methods and identity operators.py
@@ -0,0 +1,4 @@
+submitted = ["Alice", "Bob", "Charlie", "David"]
+attended = ["Charlie", "Eve", "Alice", "Frank"]
+
+print("Alice submitted her assignment and attended class.") if "Alice" in submitted and "Alice" in attended else print("Alice has not submitted assignments or attended class.")
